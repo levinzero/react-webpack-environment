728a8b7bf6c884ec32a700275608ab1d4978a51b
jie <wujiejie@codemao.cn>
2020-08-19 16:40:11 +0800

    4、babel编译ES6,JSX等

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-    mode: 'development',
-    entry: ['./src/index.js'],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {},
-    plugins: [],
-    devServer: {}
+  mode: 'development',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader'
+      }],
+    }, ]
+
+  },
+  plugins: [],
+  devServer: {}
 }
